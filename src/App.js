45840851b6d96296155f14a36836a5fd33eee2da
Luis Miguel Barthaburu <barthaburuluismiguel@outlook.com>
2022-06-16 21:13:57 -0300

    formulario de contacto funcionando con emailjs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Contacto from './componentest/Contacto/Contacto';
 import Hero from './componentest/Hero/Hero';
 
 function App() {
   return (
     <>
      <Hero/>
+     <Contacto/>
     </>
   );
 }
