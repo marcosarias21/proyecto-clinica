f9ecce7526da0acd84d145a601814fe696f12643
Agustin Valdez <agustinv45@gmail.com>
2022-06-20 18:48:37 -0300

    se agrega navbar, footer, boton

---
+++ src/componentest/Boton1/Boton1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './boton1.css';
+
+const Boton1 = (props) => {
+    return (
+        <div>
+            <button class="btn btn-3" type="submit"> {props.name} </button>
+        </div>
+    );
+};
+
+export default Boton1;
