1a0d4f5522d944d7cf0c82f5bea6ef44d248ca7a
Agustin Valdez <agustinv45@gmail.com>
2022-06-20 18:25:23 -0300

    se agrega footer, navbar y boton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Footer from './componentest/Footer/Footer';
 import Hero from './componentest/Hero/Hero';
+import Nabvar from './componentest/Navbar/Navbar';
 
 function App() {
   return (
     <>
+    <Nabvar/>
      <Hero/>
+     <Footer/>
     </>
   );
 }
