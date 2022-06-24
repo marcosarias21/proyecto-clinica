a2a9dd247f03128d5f978a34f29de3d214c1e64d
MATIAS TORRES <matiiiastorres@gmail.com>
2022-06-23 21:11:51 -0300

    borre cambios en otra pag que hice nada que ver

--- src/pages/Turnos/Turnos.js
+++ src/pages/Turnos/Turnos.js
@@ -4,23 +4,7 @@ import "./turnos.css"
 function Turnos() {
   return (
     <div>
-       <div>
-      <div classNameName="error-page">
-        <div classNameName="box">
-          <h2>
-            4<span classNameName="zero"></span>4
-          </h2>
-        </div>
-      </div>
-
-
-
-
-
-
       
-      
-    </div>
     </div>
   )
 }
