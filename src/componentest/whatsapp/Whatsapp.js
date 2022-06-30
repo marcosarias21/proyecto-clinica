f921505b1592e2d85c7c5c58d7988413847a6cc6
MATIAS TORRES <matiiiastorres@gmail.com>
2022-06-29 21:55:16 -0300

    agrege seccion especialidades y cambie gif de whatsapp

--- src/componentest/whatsapp/Whatsapp.js
+++ src/componentest/whatsapp/Whatsapp.js
@@ -4,7 +4,7 @@ import "../whatsapp/whatsapp.css"
 const Whatsapp = () => {
     return (
       <a href="https://api.whatsapp.com/send?phone=3812508502&text=Hola%20en%20que%20podemos%20ayudarte. " className="w-url" >
-          <img src="https://media.giphy.com/media/QtvHd6lI9q4iydr59B/giphy.gif" className="w-img"></img>
+          <img src="https://cliply.co/wp-content/uploads/2021/08/372108180_WHATSAPP_ICON_400.gif" className="w-img"></img>
         </a>
     )
   }
--- src/pages/LogInAdmin/LogInAdmin.js
+++ src/pages/LogInAdmin/LogInAdmin.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import yo from "../../Assets/Imagen medicos.png";
+import Contacto from "../../componentest/Contacto/Contacto";
+import Nabvar from "../../componentest/Navbar/Navbar";
 
-function LogInAdmin() {
+import "../../pages/LogInAdmin/loginAdmin.css";
+
+const LogInAdmin = () => {
   return (
-    <div>LogInAdmin</div>
-  )
-}
+    <div>
+      <Nabvar />
 
-export default LogInAdmin
+      <div className="L">
+        <div className="L-left">
+          <div className="L-bg"></div>
+          <div className="L-form">
+            <form>
+              <input
+                type="text"
+                placeholder="NOMBRE DE USUARIO"
+                name="user_name"
+              />
+              <input type="text" placeholder="E-MAIL" name="user_email" />
+              <input type="text" placeholder="D-N-I" name="user_email" />
+              <input
+                type="text"
+                placeholder="FECHA DE NACIMIENTO"
+                name="user_email"
+              />
+              <input type="text" placeholder="DIRECCION" name="user_email" />
+              <input type="text" placeholder="PROVINCIA" name="user_email" />
+              <input type="text" placeholder="TELEFONO" name="user_email" />
+            </form>
+            <button className="L-button">EDITAR</button>
+          </div>
+        </div>
+
+        <div className="L-right">
+          <div className="L-img-container">
+            <img src={yo} alt="" className="L-img" />
+          </div>
+        </div>
+      </div>
+      <Contacto />
+    </div>
+  );
+};
+
+export default LogInAdmin;
