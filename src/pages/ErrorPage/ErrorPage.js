f7270ae3dc561ec47507315aa4f1c3207138b4e2
MATIAS TORRES <matiiiastorres@gmail.com>
2022-07-04 20:22:33 -0300

    modificaciones

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -9,11 +9,15 @@ function ErrorPage() {
         <div>
           <img src={Logo} className="img-404" alt="4445" />
         </div>
-        <div className="box">
-          <h2>
+        <div className="box1">
+          <h2 className="box2">
+
+           
             4<span className="zero"></span>4
           </h2>
+         
         </div>
+        <h2 className="box3">404</h2>
 
         <div className="h3">
           <h3 >Sorry! Page not found</h3>
